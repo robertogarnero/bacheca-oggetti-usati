dd262f88da45568094900189e6fd5403e20ab6d7
EC2 Default User <ec2-user@ip-172-31-91-51.ec2.internal>
2019-05-20 06:55:06 +0000

    fase 3

--- bacheca/src/app/app.module.ts
+++ bacheca/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ElencoComponent } from './elenco/elenco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElencoComponent
   ],
   imports: [
     BrowserModule, ReactiveFormsModule, FormsModule, NgbModule
